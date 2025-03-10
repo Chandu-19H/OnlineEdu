b6f647f4eb376bd113dc00b1fc144b98df3be33d
2387868 <2387868@LTIN555471>
2025-03-10 11:13:38 +0530

    Revert "likhil intial"
    This reverts commit ead8feb7bf7d2dcc4ac5f210f85ad35316d3b2a0.

---
+++ OnlineEdu/Controllers/UserController1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineEdu.Data;
+
+namespace OnlineEdu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController1: ControllerBase
+    {
+        private readonly CoursePortalDbContext _context;
+
+        public UserController1(CoursePortalDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] User user)
+        {
+            if (user == null)
+            {
+                return BadRequest("User is null.");
+            }
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = user.UserID }, user);
+        }
+
+        [HttpGet]
+        public ActionResult<List<User>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetById(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return user;
+        }
+    }
+}
